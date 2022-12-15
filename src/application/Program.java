4b962345167baffb2cce281a02cdf987f13b48c6
viniciuspch <vinicius.geral2018@gmail.com>
2022-12-15 15:15:04 -0300

    Added options list, and swith case

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,46 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import controller.ReservationController;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		ReservationController controller = new ReservationController();
+		int option = 0;
+		do {
+
+			System.out.println("-------------Hotel---------------");
+			System.out.println("| Option 1 - New reservation    |");
+			System.out.println("| Option 2 - Update reservation |");
+			System.out.println("| Option 3 - View reservations  |");
+			System.out.println("| Option 4 - Quit               |");
+			System.out.println("---------------------------------");
+			System.out.println("Choose an option: ");
+			try {
+				option = sc.nextInt();
+			} catch (InputMismatchException e) {
+				option = 0;
+				System.out.println("Only numbers");
+			}
+
+			switch (option) {
+			
+			case 1:
+				controller.addReservation();
+				break;
+			case 2:
+				controller.updateReservation();
+				break;
+			case 3:
+				controller.viewReservations();
+				break;
+			}
+		} while (option != 4);
+		System.out.println("------Closed aplication------");
+		sc.close();
 	}
-
 }
