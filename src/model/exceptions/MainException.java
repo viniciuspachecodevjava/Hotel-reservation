98e7479c875e3147529e25d506f1d1ee0f3f7b72
viniciuspch <vinicius.geral2018@gmail.com>
2022-12-15 15:15:32 -0300

    Added exceptions file

--- src/model/exceptions/MainException.java
+++ src/model/exceptions/MainException.java
@@ -1,6 +1,11 @@
 package model.exceptions;
 
 
-public class MainException {
+public class MainException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
+	public MainException(String msg) {
+		super(msg);
+	}
 }
